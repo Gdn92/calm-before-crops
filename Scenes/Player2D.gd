1ca441729716d1e5918b362d783f7740ee1d8aa5
gdnkei0 <misakisan2019@gmail.com>
2024-09-09 21:14:02 +0800

    updated anims w idle + walk

--- Scenes/Player2D.gd
+++ Scenes/Player2D.gd
@@ -4,21 +4,26 @@ extends CharacterBody2D
 @export var friction = 0.01
 @export var acceleration = 0.1
 
+# Defining screen size, etc
+@export var screen_size : Vector2 
+
 @onready var _animated_sprite = $AnimatedSprite2D
+
+var last_direction: Vector2 = Vector2.ZERO  # Store the last movement direction
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size 
+	position = screen_size / 2
 
 func get_input():
 	var input = Vector2()
 	if Input.is_action_pressed('right'):
-		_animated_sprite.play("walk_right")
 		input.x += 1
 	if Input.is_action_pressed('left'):
-		_animated_sprite.play("walk_left")
 		input.x -= 1
 	if Input.is_action_pressed('down'):
-		_animated_sprite.play("walk_down")
 		input.y += 1
 	if Input.is_action_pressed('up'):
-		_animated_sprite.play("walk_up")
 		input.y -= 1
 	return input
 
@@ -26,7 +31,33 @@ func _physics_process(delta):
 	var direction = get_input()
 	if direction.length() > 0:
 		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
+		last_direction = direction
+		play_walk_animation(direction)  # Play the walking animation
 	else:
-		# velocity = velocity.lerp(Vector2.ZERO, friction)
 		velocity = Vector2.ZERO
+		play_idle_animation(last_direction)  # Play idle animation based on last direction
+
 	move_and_slide()
+	
+	# Limit movement to window size 
+	position = position.clamp(Vector2.ZERO, screen_size) 
+
+func play_walk_animation(direction: Vector2):
+	if direction.x > 0 and _animated_sprite.animation != "walk_right":
+		_animated_sprite.play("walk_right")
+	elif direction.x < 0 and _animated_sprite.animation != "walk_left":
+		_animated_sprite.play("walk_left")
+	elif direction.y > 0 and _animated_sprite.animation != "walk_down":
+		_animated_sprite.play("walk_down")
+	elif direction.y < 0 and _animated_sprite.animation != "walk_up":
+		_animated_sprite.play("walk_up")
+
+func play_idle_animation(direction: Vector2):
+	if direction.x > 0 and _animated_sprite.animation != "idle_right":
+		_animated_sprite.play("idle_right")
+	elif direction.x < 0 and _animated_sprite.animation != "idle_left":
+		_animated_sprite.play("idle_left")
+	elif direction.y > 0 and _animated_sprite.animation != "idle_down":
+		_animated_sprite.play("idle_down")
+	elif direction.y < 0 and _animated_sprite.animation != "idle_up":
+		_animated_sprite.play("idle_up")
